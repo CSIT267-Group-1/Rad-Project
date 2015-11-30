96a723239eea0e226e165c2da25a9daa4944e2cd
Koby Samuel <ksamuel890@gmail.com>
2015-11-30 16:19:35 -0500

    first commit. Added all files and folders for the project

---
+++ RadApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RadApp
+//
+//  Created by Koby Samuel on 11/30/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ RadApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RadApp
+//
+//  Created by Koby Samuel on 11/30/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
