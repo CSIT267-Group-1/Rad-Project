51e2e604da54cf1a88abecfd6f3d1a4d512eede6
Matt Matuk <coolskater6088@hotmail.com>
2015-11-30 19:43:04 -0500

    Starting creating teh quiz pa view controller

--- RadApp/FirstViewController.swift
+++ RadApp/FirstViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    @IBOutlet var modelSeg: UISegmentedControl!
+    
     @IBAction func determineQuiz(sender: AnyObject) {
+        let segSelection: Int = modelSeg.selectedSegmentIndex
+        let mainStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+
+        switch (segSelection)
+        {
+        case 0:
+            let editorVC = mainStoryBoard.instantiateViewControllerWithIdentifier("QuizAP")
+            editorVC.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+            presentViewController(editorVC, animated: true, completion: nil)
+        default:
+            print("Failed to start quiz")
+        }
     }
     
 	override func viewDidLoad() {
---
+++ RadApp/QuizPAViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QuizPAViewController.swift
+//  RadApp
+//
+//  Created by csit267-8 on 11/30/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+
+import UIKit
+
+class QuizPAViewController: UIViewController {
+
+    @IBAction func dismissQuiz(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
