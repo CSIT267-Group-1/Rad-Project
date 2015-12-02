f019d47b77d3a3df968bfe818311093d93752a6b
Koby Samuel <ksamuel890@gmail.com>
2015-12-02 16:53:04 -0500

    fixed some things (again)

---
+++ RadApp/QuizAPViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizAPViewController.swift
+//  RadApp
+//
+//  Created by Koby Samuel on 12/2/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+
+import UIKit
+
+class QuizAPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
