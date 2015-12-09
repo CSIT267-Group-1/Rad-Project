e4ad7d127465ded38b1648e9069794cb2a93d3f9
Matt Matuk <coolskater6088@hotmail.com>
2015-12-09 18:34:22 -0500

    Added more to the PA Quiz

---
+++ RadApp/Quiz.swift
@@ -0,0 +1,47 @@
+//
+//  Quiz.swift
+//  RadApp
+//
+//  Created by csit267-8 on 12/9/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+//  Creates a quiz class that conatins:
+//      1 - Question
+//      2 - Boolean to determine if the question is multiple choice
+//      3 - Correct Button 
+//      4 - Titles for all the multiple choice questions
+//
+
+import UIKit
+
+class Quiz
+{
+    var question: String!
+    var hasMultipleChoice: Bool!
+    var correctButton: UIButton!
+    var buttonATitle: String!
+    var buttonBTitle: String!
+    var buttonCTitle: String!
+    var buttonDTitle: String!
+    
+    init (question: String, hasMultipleChoice: Bool, correctButton: UIButton, buttonArray: [String]?)
+    {
+        self.question=question
+        self.hasMultipleChoice=hasMultipleChoice
+        self.correctButton=correctButton
+        
+        if (buttonArray != nil)
+        {
+            setButtonTitles(buttonArray!)
+        }
+    }
+    
+    func setButtonTitles(buttonArray: [String])
+    {
+        buttonATitle=buttonArray[0]
+        buttonBTitle=buttonArray[1]
+        buttonCTitle=buttonArray[2]
+        buttonDTitle=buttonArray[3]
+    }
+
+}
--- RadApp/QuizPAViewController.swift
+++ RadApp/QuizPAViewController.swift
@@ -19,10 +19,21 @@ class QuizPAViewController: UIViewController {
     @IBOutlet var sigmoid: UIButton!
     @IBOutlet var descCol: UIButton!
     @IBOutlet var ascCol: UIButton!
+    @IBOutlet var question: UILabel!
+    @IBOutlet var questionNumber: UILabel!
+    @IBOutlet var submit: UIButton!
+    @IBOutlet var multipleChoiceButtonA: UIButton!
+    @IBOutlet var multipleChoiceButtonB: UIButton!
+    @IBOutlet var multipleChoiceButtonC: UIButton!
+    @IBOutlet var multipleChoiceButtonD: UIButton!
+    @IBOutlet var multipleChoiceView: UIStackView!
     
-    var questions: [String: UIButton]!
+    var questions: [Quiz] = [Quiz]()
     
     @IBAction func locationSelected(sender: AnyObject) {
+        
+        // sets the setelced location to so the user can see where they clicked. 
+        // Also removes any other current selection
         if currentSelectedArea != nil
         {
             currentSelectedArea.backgroundColor=UIColor.clearColor()
@@ -45,7 +56,8 @@ class QuizPAViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        //displayRandomQuestion()
+        createQuestions()
+        displayQuestion(7)
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,24 +67,52 @@ class QuizPAViewController: UIViewController {
     
     func createQuestions()
     {
-        questions =
-        [
-            "What is the location of the splenic flexure on a PA view of a barium enema?": spleFlex,
-            "What is the location of the hepatic flexure on a PA view of a barium enema?": hepFelx,
-            "What is the location of the descending colon on a PA view of a barium enema?": descCol,
-            "What is the location of the ascending colon on a PA view of a barium enema?": ascCol,
-            "What is the location of the transverse colon on a PA view of a barium enema?": tranCol,
-            "In a PA view of a barium enema, Is the transverse colon filled with ______________?": UIButton(),
-            "In a PA axial oblique (RAO) projection of a barium enema view, what is the anatomy that is being best demonstrated?": UIButton(),
-            "In a PA axial projection of a barium enema view, what is the anatomy that is being best demonstrated?": UIButton()
-        ]
+        var multipleChoiceArray: [String] // used to store multiple choice buton titles
+
+        questions.append(Quiz(question: "What is the location of the splenic flexure on a PA view of a barium enema?", hasMultipleChoice: false, correctButton: spleFlex, buttonArray: nil))
+        questions.append(Quiz(question: "What is the location of the hepatic flexure on a PA view of a barium enema?", hasMultipleChoice: false, correctButton: hepFelx, buttonArray: nil))
+        questions.append(Quiz(question: "What is the location of the descending colon on a PA view of a barium enema?", hasMultipleChoice: false, correctButton: descCol, buttonArray: nil))
+        questions.append(Quiz(question: "What is the location of the ascending colon on a PA view of a barium enema?", hasMultipleChoice: false, correctButton: ascCol, buttonArray: nil))
+        questions.append(Quiz(question: "What is the location of the transverse colon on a PA view of a barium enema?", hasMultipleChoice: false, correctButton: tranCol, buttonArray: nil))
+        
+        multipleChoiceArray = ["Air", "Barium", "Both air and Barium", "Nothing"]
+        questions.append(Quiz(question: "In a PA view of a barium enema, Is the transverse colon filled with ______________?", hasMultipleChoice: true, correctButton: multipleChoiceButtonB, buttonArray: multipleChoiceArray))
+        
+        multipleChoiceArray = ["Ascending Colon", "Descending Colon", "Sigmoid Colon", "Transverse Colon"]
+        questions.append(Quiz(question: "In a PA axial oblique (RAO) projection of a barium enema view, what is the anatomy that is being best demonstrated?", hasMultipleChoice: true, correctButton: multipleChoiceButtonC, buttonArray: multipleChoiceArray))
+        
+        multipleChoiceArray = ["Ascending Colon", "Descending Colon", "Sigmoid Colon", "Transverse Colon"]
+        questions.append(Quiz(question: "In a PA axial projection of a barium enema view, what is the anatomy that is being best demonstrated?", hasMultipleChoice: true, correctButton: multipleChoiceButtonC, buttonArray: multipleChoiceArray))
+        
+        // Makes the questions random
+        shuffleQuestions()
     }
     
-    func displayRandomQuestion()
+    func shuffleQuestions()
     {
-        for var count = 0; count < questions.count; count++
+        
+    }
+    
+    func displayQuestion(number: Int)
+    {
+        // Sets the questions labels and number
+        question.text=questions[number].question
+        questionNumber.text="# \(number+1)"
+        
+        // displays the multiple choice buttons if the question has them
+        if questions[number].hasMultipleChoice == true
         {
+            multipleChoiceView.hidden=false
             
+            multipleChoiceButtonA.setTitle(questions[number].buttonATitle, forState: UIControlState.Normal)
+            multipleChoiceButtonB.setTitle(questions[number].buttonBTitle, forState: UIControlState.Normal)
+            multipleChoiceButtonC.setTitle(questions[number].buttonCTitle, forState: UIControlState.Normal)
+            multipleChoiceButtonD.setTitle(questions[number].buttonDTitle, forState: UIControlState.Normal)
+
+        }
+        else
+        {
+            multipleChoiceView.hidden=true
         }
     }
 
