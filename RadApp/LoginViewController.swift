90d917211232fe374bb6566838fff4fcd44ad40b
Koby Samuel <ksamuel890@gmail.com>
2015-12-14 16:55:14 -0500

    fixed login (next step - storyboaard)

--- RadApp/LoginViewController.swift
+++ RadApp/LoginViewController.swift
@@ -13,17 +13,16 @@ class LoginViewController: UIViewController {
 	@IBOutlet weak var lastNameField: UITextField!
 	@IBOutlet weak var studentIDField: UITextField!
 	@IBOutlet weak var submitButton: UIButton!
-	var firstName: String!
-	var lastName: String!
-	var studentID: Int!
+	let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+	
 
-	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+/**	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 	}
 
 	required init?(coder aDecoder: NSCoder) {
 	    fatalError("init(coder:) has not been implemented")
-	}
+	} */
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,15 +42,15 @@ class LoginViewController: UIViewController {
 			self.presentViewController(errorController, animated: true, completion: nil)
 		}
 		else {
-			self.firstName = firstNameField.text! as String
-			self.lastName = lastNameField.text! as String
+			appDelegate.firstName = firstNameField.text! as String
+			appDelegate.lastName = lastNameField.text! as String
 			if(Int(studentIDField.text!) == nil) {
 				let errorController = UIAlertController(title: "Error!", message: "Please enter only numbers in the \"Student ID #\" field", preferredStyle: UIAlertControllerStyle.Alert)
 				errorController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: nil))
 				self.presentViewController(errorController, animated: true, completion: nil)
 			}
 			else {
-				self.studentID = Int(studentIDField.text! as String)!
+				appDelegate.studentID = Int(studentIDField.text! as String)!
 			}
 			return true
 		}
