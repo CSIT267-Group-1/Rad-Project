032e7041c5cf218b5e9e97cff3a8b437941d0b6a
Koby Samuel <ksamuel890@gmail.com>
2015-12-16 15:31:39 -0500

    grammatical fixings of the array for the instructions

--- RadApp/InstructionsViewController.swift
+++ RadApp/InstructionsViewController.swift
@@ -12,7 +12,7 @@ class InstructionsViewController: UIViewController {
 
     var instructions =
     [
-    "1 – The Quiz will display up to 10 different question for you to answer./n/n2 – There will be two different types of questions that can be shown./n/tA) - The first type will display four multiple choice answer for you to choose./n/t   - Tap one of the four answers to pick your answer./n/tB) – The second type will not display any multiple choice answer and will instead ask you to tab a location on the picture./n/n3 – After choosing an answer, tap the submit button to see if your answer was correct./n/n4 – A messaged will be displayed with the result of your answer./n/n5 – The correct answer will be highlighted in green./n/n6 – If your answer was wrong, you will see you answer chosen as red and the correct answer highlighted in green."
+    "1. The Quiz will display up to 10 different question for you to answer./n/n2. There will be two different types of questions that can be shown./n/tA) The first type will display four multiple choice answer for you to choose./n/t   - Tap one of the four answers to pick your answer./n/tB) The second type will not display any multiple choice answer and will instead ask you to tab a location on the picture./n/n3. After choosing an answer, tap the submit button to see if your answer was correct./n/n4. A messaged will be displayed with the result of your answer./n/n5. The correct answer will be highlighted in green./n/n6. If your answer was wrong, you will see you answer chosen as red and the correct answer highlighted in green."
     ]
     
     @IBAction func dimiss(sender: AnyObject) {
